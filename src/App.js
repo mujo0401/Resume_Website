b3a98c2467bcb9b5f679a03163a396cc768bdbb1
mujo0401 <42476372+mujo0401@users.noreply.github.com>
2024-05-20 16:49:52 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Resume_Website">
      <Navigation /> 
       <Routes>
         <Route path="/" element={<Home />} />
