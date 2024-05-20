d10357f1c85254cc73e081fbae5505b69afe8648
mujo0401 <42476372+mujo0401@users.noreply.github.com>
2024-05-20 16:45:21 -0500

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Education from './pages/Education';
 import Volunteer from './pages/Volunteer';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
