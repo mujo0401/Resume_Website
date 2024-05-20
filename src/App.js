bb54b3c0d9ffaf2037b626ae715eec561c23652e
mujo0401 <42476372+mujo0401@users.noreply.github.com>
2024-05-20 17:02:32 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Education from './pages/Education';
 import Volunteer from './pages/Volunteer';
-import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <Router basename="/Resume_Website">
-     <Navigation /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
